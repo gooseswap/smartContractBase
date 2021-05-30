c1d31dd4fe054449fac6aaa87560930f96bb062a
code6449 <85029525+code6449@users.noreply.github.com>
2021-05-29 17:53:05 -0700

    Update auth.sol

--- auth.sol
+++ auth.sol
@@ -1,39 +1,57 @@
-pragma solidity ^0.7.4;
+pragma solidity ^0.6.12; // #####  SPDX-License-Identifier: None
+
 
 abstract contract auth {
-    address payable _owner;
+    address _owner;
     mapping (address => bool) _whiteList;
     mapping (address => bool) _blackList;
     
-    constructor() { 
+    constructor() public{ 
         _owner = msg.sender; 
         _whiteList[msg.sender] = true;
     }
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
+        return msg.data;
+    }    
     function isOwner(address account) public view returns (bool) {
         return account == _owner;
     }
+    
+    function isAuth(address account) public view returns (bool) {
+        return _whiteList[account];
+    }
+
+    function isBanned(address account) public view returns (bool) {
+        return _blackList[account];
+    }
+    
     modifier onlyOwner() {
-        require(isOwner(msg.sender)); _;
+        require(isOwner(_msgSender())); _;
     }
-    modifier auth() {
-        require(_whiteList[msg.sender] == true); _;
+    modifier _auth() {
+        require(_whiteList[_msgSender()] == true); _;
     }
-    modifier banList() {
-        require(_blackList[msg.sender] == false); _;
+    modifier _banCheck() {
+        require(_blackList[_msgSender()] == false); _;
     }
-    function authorize(address adr) public owned {
-        _whitelist[adr] = true;
+    function authorize(address adr) public onlyOwner {
+        _whiteList[adr] = true;
     }
-    function unauthorize(address adr) external owned {
-        _whitelist[adr] = false;
+    function unauthorize(address adr) external onlyOwner {
+        _whiteList[adr] = false;
     }
-   function blackList(address adr) public owned {
+   function blackList(address adr) public onlyOwner {
         _blackList[adr] = true;
     }
-    function unBlackList(address adr) external owned {
+    function unBlackList(address adr) external onlyOwner {
         _blackList[adr] = false;
     }
-    function transferOwnership(address payable adr) public owned {
+    function transferOwnership(address payable adr) public onlyOwner {
         _owner = adr;
     }
 }
